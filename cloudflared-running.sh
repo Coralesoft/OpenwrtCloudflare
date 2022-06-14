7467e47595194b50895573bed2102f3c3a07277f
Coraledev <82213665+Coralesoft@users.noreply.github.com>
2022-06-14 20:38:00 +1200

    Update cloudflared-running.sh

--- cloudflared-running.sh
+++ cloudflared-running.sh
@@ -1,11 +1,11 @@
 #!/bin/sh /etc/rc.common
 # Cloudflared is running check
-# run this as a service to check if cloudflare is running
+# run this as a service to check if cloudflare is update
 # Setup a cron job to do this as a scheduled task
-# example Run every 15 minutes
-# */15  * * * * /usr/sbin/cloudflared-running.sh
-# Example run every 10 minutes
-# */10  * * * * /usr/sbin/cloudflared-running.sh
+# example Run at 11:38 am each day
+# 38 11 * * * /usr/sbin/cloudflared-update.sh
+# Example run at midnight each day
+# 0 0 * * * /usr/sbin/cloudflared-update.sh
 #
 echo " "
 echo "***************************************************"
@@ -17,14 +17,15 @@ echo " "
 PID=$(pidof cloudflared)
 if [ -z "$PID" ]
 then
-      echo "Cloudflare is down"
-	  echo "Restarting Cloudflare"
-	  echo " "
-	  /etc/init.d/cloudflared start
-	  echo " "
+        echo "Cloudflare is down"
+        echo "Restarting Cloudflare"
+        echo " "
+        /etc/init.d/cloudflared start
+        echo " "
 
 else
-      echo "Cloudflare is Up"
-	  echo " "
+        echo "Cloudflare is Up pid: "$PID
+        echo " "
 fi
 
+exit 0
