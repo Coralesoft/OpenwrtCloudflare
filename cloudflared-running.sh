9f0220aeab3adeeaa7b0ebb7b258514b7a508edc
Coraledev <82213665+Coralesoft@users.noreply.github.com>
2022-06-14 19:25:39 +1200

    Add files via upload

---
+++ cloudflared-running.sh
@@ -0,0 +1,30 @@
+#!/bin/sh /etc/rc.common
+# Cloudflared is running check
+# run this as a service to check if cloudflare is running
+# Setup a cron job to do this as a scheduled task
+# example Run every 15 minutes
+# */15  * * * * /root/cloudflared-running.sh
+# Example run every 10 minutes
+# */10  * * * * /root/cloudflared-running.sh
+#
+echo " "
+echo "***************************************************"
+echo "**      Checking if cloudflared is running       **"
+echo "** github.com/Coralesoft/PiOpenwrtCloudflare     **"
+echo "***************************************************"
+# commands to update cloidflared tunnel
+echo " "
+PID=$(pidof cloudflared)
+if [ -z "$PID" ]
+then
+      echo "Cloudflare is down"
+	  echo "Restarting Cloudflare"
+	  echo " "
+	  /etc/init.d/cloudflared start
+	  echo " "
+
+else
+      echo "Cloudflare is Up"
+	  echo " "
+fi
+
