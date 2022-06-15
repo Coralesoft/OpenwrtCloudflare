82143e90dabf9678308c670e41c1a7fe22c0c0c1
Coraledev <82213665+Coralesoft@users.noreply.github.com>
2022-06-15 19:05:08 +1200

    Update cloudflared-running.sh

--- cloudflared-running.sh
+++ cloudflared-running.sh
@@ -1,31 +1,37 @@
 #!/bin/sh /etc/rc.common
-# Cloudflared is running check
-# run this as a service to check if cloudflare is update
+# cloudflare-running.sh
+# Script to check if cloudflared is running and restart if it has crashed
+# Copyright (C) 2022 C. Brown (dev@coralesoft)
+# GNU General Public License
+# Last revised 15/06/2022
+# version 1.0
+# 
+# run this as a service to check if cloudflare is running
 # Setup a cron job to do this as a scheduled task
-# example Run at 11:38 am each day
-# 38 11 * * * /usr/sbin/cloudflared-update.sh
-# Example run at midnight each day
-# 0 0 * * * /usr/sbin/cloudflared-update.sh
+# example Run every 15 minutes
+# */15  * * * * /root/cloudflared-running.sh
+# Example run every 10 minutes
+# */10  * * * * /root/cloudflared-running.sh
 #
 echo " "
 echo "***************************************************"
 echo "**      Checking if cloudflared is running       **"
 echo "** github.com/Coralesoft/PiOpenwrtCloudflare     **"
 echo "***************************************************"
-# commands to update cloidflared tunnel
+# commands to update cloudflared tunnel
 echo " "
 PID=$(pidof cloudflared)
 if [ -z "$PID" ]
 then
-        echo "Cloudflare is down"
-        echo "Restarting Cloudflare"
-        echo " "
-        /etc/init.d/cloudflared start
-        echo " "
+      echo "Cloudflare is down"
+	  echo "Restarting Cloudflare"
+	  echo " "
+	  /etc/init.d/cloudflared start
+	  echo " "
 
 else
-        echo "Cloudflare is Up pid: "$PID
-        echo " "
+      echo "Cloudflare is Up"
+	  echo " "
 fi
 
 exit 0
