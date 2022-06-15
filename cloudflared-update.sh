cc4684508501b1973407212d96eed212e213cb61
Coraledev <82213665+Coralesoft@users.noreply.github.com>
2022-06-15 18:59:43 +1200

    Update cloudflared-update.sh

--- cloudflared-update.sh
+++ cloudflared-update.sh
@@ -1,17 +1,24 @@
 #!/bin/sh /etc/rc.common
-# Cloudflared daemon update
+# Cloudflared install
+# Script to update the Cloudflare binary
+# Copyright (C) 2022 C. Brown (dev@coralesoft)
+# GNU General Public License
+# Last revised 15/06/2022
+# version 1.0
+#
 # run this as a service to regularly update or call as needed
 # Setup a cron job to do this as a scheduled task
 # example Run at 11:38 am each day
-# 38 11 * * * /usr/sbin/cloudflared-update.sh
+# 38 11 * * * /root/cloudflared-update.sh
 # Example run at midnight each day
-# 0 0 * * * /usr/sbin/cloudflared-update.sh
+# 0 0 * * * /root/cloudflared-update.sh
+# note pull checksum and test dont download if same
 #
 echo "***************************************************"
 echo "**             updating cloudflared              **"
 echo "** github.com/Coralesoft/PiOpenwrtCloudflare     **"
 echo "***************************************************"
-# commands to update cloidflared tunnel
+# commands to update cloudflared tunnel
 echo " "
 msgf="Killing current tunnel pid "
 PID=$(pidof cloudflared)
@@ -37,9 +44,9 @@ echo  " "
 echo "Changing permisions complete"
 echo " "
 echo "Restarting the tunnel"
-echo " "
+
 /etc/init.d/cloudflared start
-echo " "
+
 echo "Upgrade has been completed"
 echo "***************************************************"
 
