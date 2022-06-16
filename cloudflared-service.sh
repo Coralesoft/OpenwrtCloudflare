b88d14e33c5772878930a95d17e8845f5a75161e
Coraledev <82213665+Coralesoft@users.noreply.github.com>
2022-06-16 18:22:53 +1200

    Update cloudflared-service.sh

--- cloudflared-service.sh
+++ cloudflared-service.sh
@@ -15,6 +15,7 @@
 ##	https://github.com/Coralesoft/PiOpenwrtCloudflare
 ##							
 #######################################################################
+USE_PROCD=1
 
 START=38
 STOP=50
@@ -23,32 +24,23 @@ RESTART=55
 # fix the cf buffer issues
 sysctl -w net.core.rmem_max=2500000
 
-#start commands
-start() {
-        echo starting
-        # Start the cloudflare service commands to launch cloidflared tunnel
-		# Supplying the config directory and tunnel name plus log file.
-		# Update the config and tunnel name to suit your setup
-		#/usr/bin/cloudflared tunnel --config <<CONFIG LOCATION>> run <<TUNNELNAME>> &> /root/.cloudflared/<<LOGFILE>> &
-        
-		/usr/bin/cloudflared tunnel --config /root/.cloudflared/config.yml run OpenTun &> /root/.cloudflared/tunnellogs.txt &
-        
-		# execute the tunnel and log to the tunnellogs file
+start_service() {
+    procd_open_instance
+    procd_set_param command /usr/sbin/cloudflared tunnel --config /root/.cloudflared/config.yml run OpenTun &> /root/.cloudflared/tunnellogs.txt &
+    procd_set_param stdout 1
+    procd_set_param stderr 1
+    procd_set_param respawn ${respawn_threshold:-3600} ${respawn_timeout:-5} ${respawn_retry:-5}
+    procd_set_param user
+    procd_close_instance
 }
 
-stop() {
-        echo stopping
-        # Kill the running tunnel
-        killall -9 cloudflared
+stop_service() {
+	killall -9 cloudflared
 }
-restart() {
-		# restart for the service
-        echo restarting
-        killall -9 cloudflared
-		# give it time to clean up
-        sleep 2
-		# Start the service
-        /usr/bin/cloudflared tunnel --config /root/.cloudflared/config.yml run OpenTun &> /root/.cloudflared/tunnellogs.txt &
+
+reload_service(){
+	stop
+	start
 }
 
 # end of script
