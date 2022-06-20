735c00d5459bab5b302b2d06b626d12d6e21745f
Coraledev <82213665+Coralesoft@users.noreply.github.com>
2022-06-20 20:03:39 +1200

    Update uninstall-cloudflared.sh

--- uninstall-cloudflared.sh
+++ uninstall-cloudflared.sh
@@ -7,12 +7,27 @@
 # version 2202.06.18
 #
 echo " "
-echo "Removing Cloudfared and its condif" 
+echo "Removing Cloudfared and its config"
 echo " "
+echo "Stopping the current tunnel"
+/etc/init.d/cloudflared stop
+cloudflared tunnel list
+read -p "Enter your tunnel name for deletion: " TUNNAME
+echo " "
+echo "Deleting tunnel: "$TUNNAME
+/usr/sbin/cloudflared tunnel delete $TUNNAME
+echo " "
+echo"Removing config"
 rm /root/.cloudflared/*
+echo "Removing Service"
 rm /etc/init.d/cloudflared
+echo "Removing updates"
 rm /usr/sbin/cloudflared-update
+echo "Removing Deamon"
 rm /usr/sbin/cloudflared
+echo "Remving crontab"
 crontab -l | grep -v '/usr/sbin/cloudflared-update' | crontab -
-exit 0
+echo " "
+echo "Uninstall completed"
 
+exit 0
