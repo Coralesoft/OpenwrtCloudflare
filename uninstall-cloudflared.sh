3b699c744a7a2bed993674c5961574ac0f0a9d76
Coraledev <82213665+Coralesoft@users.noreply.github.com>
2022-07-02 13:17:16 +1200

    Update uninstall-cloudflared.sh

--- uninstall-cloudflared.sh
+++ uninstall-cloudflared.sh
@@ -4,13 +4,14 @@
 # Copyright (C) 2022 C. Brown (dev@coralesoft)
 # GNU General Public License
 # Last revised 21/06/2022
-# version 2022.06.8
+# version 2022.07.1
 #-----------------------------------------------------------------------
 # Version      Date         Notes:
 # 1.0                       Inital Release
 # 2022.06.2    20.06.2022   Script fixes and updates
 # 2022.06.3    21.06.2022   Script cleanup
 # 2022.06.8    21.06.2022   Multiple formatting Updates
+# 2022.07.1    02.07.2022   Cleanup
 #
 echo " "
 echo "Removing Cloudfared and its config"
@@ -36,7 +37,10 @@ rm /usr/sbin/cloudflared
 echo "Removing crontab entry"
 crontab -l | grep -v '/usr/sbin/cloudflared-update' | crontab -
 echo " "
+echo "Restarting Cron"
 /etc/init.d/cron restart
+echo " "
 echo "Uninstall completed"
+
 
 exit 0
